f193608815da4fb0b6fb1a9e7e58a23a6b50514c
nizzle <nizom7812@gmail.com>
2025-04-13 17:10:14 +0500

    - minor changes

--- analysis/hello_world.py
+++ analysis/hello_world.py
@@ -1,13 +1,13 @@
 import os
 
-# Print to stdout (this goes to the job log)
+# Print to stdout (these will go to the job log automatically)
 print("Hello World from Epsilon SDK!")
 print("This is a simple Python script execution test.")
 
 # Create the output directory if it doesn't exist
 os.makedirs("output", exist_ok=True)
 
-# Create the output file that matches your project.yaml specification
+# Create the output file in the exact location specified in project.yaml
 with open("output/hello_world.log", "w") as f:
     f.write("Hello World from Epsilon SDK!\n")
     f.write("This is a simple Python script execution test.\n")
